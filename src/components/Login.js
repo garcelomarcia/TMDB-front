2da0873feb7c051c019c36c691c9ceb33037cc74
garcelomarcia <marcelogarcia91@gmail.com>
2023-06-28 13:37:02 -0600

    switched to user

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
     try {
       const response = await axios.post(
         `https://tmdb-back-w5b3.onrender.com/api/login`,
-        { ...user }
+        user
       );
 
       const token = response.data;
