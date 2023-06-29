e883ab12cc5eb965646cc2b059e68ada602fab79
garcelomarcia <marcelogarcia91@gmail.com>
2023-06-29 10:09:13 -0600

    window location reload

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Menubar.css";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg";
 import axios from "axios";
 
 const Menubar = ({ handleSubmit, handleBrowse, isLoggedIn }) => {
-  const [logged, setLogged] = useState(isLoggedIn);
   const navigate = useNavigate();
   const handleLogout = () => {
     // document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     localStorage.removeItem("token");
-    setLogged("");
     navigate("/login");
+    window.location.reload();
   };
   return (
     <>
@@ -135,12 +134,12 @@ const Menubar = ({ handleSubmit, handleBrowse, isLoggedIn }) => {
             </div>
           </li>
           <li>
-            {!logged ? (
+            {!isLoggedIn ? (
               <Link to={"/login"}>Login</Link>
             ) : (
               <>
                 <Link to={"/login"} id="username">
-                  {logged}
+                  {isLoggedIn}
                   <i className="fas fa-caret-down"></i>
                 </Link>
                 <div className="dropdown-menu">
